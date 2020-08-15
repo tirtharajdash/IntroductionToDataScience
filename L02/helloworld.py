8b900c33b60870606a80e7655a7d22cf9d7e0c3b
tirtharajdash <dashtirtharaj@gmail.com>
2020-08-16 01:01:19 +0530

    update

---
+++ L02/helloworld.py
@@ -0,0 +1,11 @@
+#print a string within print function
+print('Hello, World!')
+
+#print a variable
+msg = 'Hello, People!'
+print(msg)
+
+#we can also print a number
+num = 100.
+print('Value of number is:', num)
+print('Type of number is:', type(num))
